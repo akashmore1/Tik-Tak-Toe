36f447da31ec5331f29b8cca84b04b0db76d0ad5
Akash More <akashkmore1@gmail.com>
2022-10-09 02:47:46 +0530

    On click x/o

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,11 +4,33 @@ import "./Main.css";
 
 const Main = () => {
   const [isXPresent, setIsXPresent] = useState(false);
+  let cellValue1 = isXPresent ? "O" : "X";
+  let cellValue2 = isXPresent ? "O" : "X";
+  let cellValue3 = isXPresent ? "O" : "X";
+
   return (
     <div className="main">
-      <Row setIsXPresent={setIsXPresent} isXPresent={isXPresent} />
-      <Row setIsXPresent={setIsXPresent} isXPresent={isXPresent} />
-      <Row setIsXPresent={setIsXPresent} isXPresent={isXPresent} />
+      <Row
+        setIsXPresent={setIsXPresent}
+        isXPresent={isXPresent}
+        cellValue1={cellValue1}
+        cellValue2={cellValue2}
+        cellValue3={cellValue3}
+      />
+      <Row
+        setIsXPresent={setIsXPresent}
+        isXPresent={isXPresent}
+        cellValue1={cellValue1}
+        cellValue2={cellValue2}
+        cellValue3={cellValue3}
+      />
+      <Row
+        setIsXPresent={setIsXPresent}
+        isXPresent={isXPresent}
+        cellValue1={cellValue1}
+        cellValue2={cellValue2}
+        cellValue3={cellValue3}
+      />
     </div>
   );
 };
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,36 +1,45 @@
 import React, { useState } from "react";
 import "./Row.css";
 
-const Row = ({ setIsXPresent }) => {
+const Row = ({
+  setIsXPresent,
+  isXPresent,
+  cellValue1,
+  cellValue2,
+  cellValue3,
+}) => {
   const [cell1_clicked, setCell1_Clicked] = useState(false);
   const [cell2_clicked, setCell2_Clicked] = useState(false);
   const [cell3_clicked, setCell3_Clicked] = useState(false);
-  let cellValue = "X";
+
   return (
     <div className="row">
       <div
         className="cell"
         onClick={() => {
           setCell1_Clicked(true);
+          setIsXPresent(!isXPresent);
         }}
       >
-        {cell1_clicked && cellValue}
+        {cell1_clicked && cellValue1}
       </div>
       <div
         className="cell"
         onClick={() => {
           setCell2_Clicked(true);
+          setIsXPresent(!isXPresent);
         }}
       >
-        {cell2_clicked && cellValue}
+        {cell2_clicked && cellValue2}
       </div>
       <div
         className="cell"
         onClick={() => {
           setCell3_Clicked(true);
+          setIsXPresent(!isXPresent);
         }}
       >
-        {cell3_clicked && cellValue}
+        {cell3_clicked && cellValue3}
       </div>
     </div>
   );
